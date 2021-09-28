0fbca03970187c681da7d4f451c5c8ad9672357b
aswinelavarasan <aswin28111998@gmail.com>
2021-09-28 16:22:58 +0530

    new command

---
+++ ecommerce/settings/prod.py
@@ -0,0 +1,24 @@
+from .base import *
+
+DATABASES = {
+       'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'shopdata',
+        'USER': 'root',  
+        'PASSWORD':'12345',  
+        'HOST':'localhost',  
+        'PORT':'3306' , 
+    
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+SECRET_KEY = 'ly19&2+c#onohxpodxnrlpvx3#)wc_qw!nvnb0^)oe%_yj$vf4'
+ALLOWED_HOSTS = ['ecommerce.pythonanywhere.com']
+
+MEDIA_ROOT = '/home/ecommerce/ecommerce/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/ecommerce/ecommerce/static'
+STATIC_URL = '/static/'
